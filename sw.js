27266f88b97a0b7dcb51e617b0bc64b730a143a8
Max Assmy <massmy@users.noreply.github.com>
2025-05-22 16:11:08 +0200

    added new features

--- sw.js
+++ sw.js
@@ -10,6 +10,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
@@ -21,4 +22,8 @@ self.addEventListener('fetch', (event) => {
       response || fetch(event.request)
     )
   );
+});
+
+self.addEventListener("activate", (event) => {
+  clients.claim();
 });
